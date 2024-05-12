88046675a0f2f528489f8d87a48b65000c47886e
jvn <jvn@peytz.dk>
2024-05-12 11:09:54 +0200

    fix: get control of envvars

--- src/utils/fetchWeather.ts
+++ src/utils/fetchWeather.ts
@@ -1,11 +1,12 @@
 const API_KEY = '8e4b29f425b574e6f7cf4805ced4daee';
 
 export const fetchWeather = async (city: string): Promise<any> => {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`,
-	{
-		next: { revalidate: 600 },
-	  }
+	const apiKey = process.env.OPEN_WEATHER_MAP_API_KEY;
+  	const response = await fetch(
+    	`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,
+		{
+			next: { revalidate: 600 },
+	  	}
   );
   return response.json();
 };
